f550ed9f7ae4d215e43b49e1d37714dc45dd3ca8
raulcoloni <rauldujvxd@gmail.com>
2024-08-07 21:54:28 -0300

    aula 2

---
+++ LOOP.py
@@ -0,0 +1,6 @@
+limite = int(input("digite uma tabuada!"))
+print("contando...")
+x = 2
+for contador in range(limite):
+ print("a soma de",contador+1," + ",x,"=",contador+1+x)
+    
---
+++ tabuada.py
@@ -0,0 +1,5 @@
+tabuada = input("qual tabuada:")
+print("multiplicando")
+x = int(tabuada)
+for multiplicar in range(10):
+   print("tabuada",multiplicar+1," * ",x,"=",multiplicar+1)*x
